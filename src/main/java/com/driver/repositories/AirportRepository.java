5e2e7025ecafbaf2ecc268bfab7662e8cd57007b
Abhishek Akkapalli <abhishekgoud.jobs@gmail.com>
2023-10-04 14:32:07 +0530

    APIs completed

---
+++ src/main/java/com/driver/repositories/AirportRepository.java
@@ -0,0 +1,66 @@
+package com.driver.repositories;
+
+import com.driver.model.Airport;
+import com.driver.model.Flight;
+import com.driver.model.Passenger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AirportRepository {
+    private HashMap<String, Airport> airportDb = new HashMap<>();
+    private HashMap<Integer, Flight> flightDb = new HashMap<>();
+    private HashMap<Integer, Passenger> passengerDb = new HashMap<>();
+    private HashMap<Integer, List<Integer>> passengerFlightDb = new HashMap<>();
+
+    public void addAirport(Airport airport) {
+        airportDb.put(airport.getAirportName(), airport);
+    }
+
+    public Airport getAirportByName(String airportName) {
+        return airportDb.get(airportName);
+    }
+
+    public List<Airport> getAllAirports() {
+        return new ArrayList<>(airportDb.values());
+    }
+
+    public void addFlight(Flight flight) {
+        flightDb.put(flight.getFlightId(), flight);
+    }
+
+    public Flight getFlightById(Integer flightId) {
+        return flightDb.get(flightId);
+    }
+
+    public List<Flight> getAllFlights() {
+        return new ArrayList<>(flightDb.values());
+    }
+
+    public void addPassenger(Integer passengerId, Passenger passenger) {
+        passengerDb.put(passengerId, passenger);
+    }
+    public Passenger getPassengerById(Integer passengerId) {
+        return passengerDb.get(passengerId);
+    }
+
+    public void bookTicket(Integer flightId, Integer passengerId) {
+        List<Integer> passengerList = passengerFlightDb.getOrDefault(flightId, new ArrayList<>());
+        passengerList.add(passengerId);
+
+        passengerFlightDb.put(flightId, passengerList);
+    }
+
+    public void cancelTicket(Integer flightId, Integer passengerId) {
+        List<Integer> passengerList = passengerFlightDb.getOrDefault(flightId, new ArrayList<>());
+        passengerList.remove(passengerId);
+
+        passengerFlightDb.put(flightId, passengerList);
+    }
+
+    public List<Integer> getPassengersByFlight(Integer flightId) {
+        return passengerFlightDb.getOrDefault(flightId, new ArrayList<>());
+    }
+}
---
+++ src/main/java/com/driver/services/AirportService.java
@@ -0,0 +1,136 @@
+package com.driver.services;
+
+import com.driver.model.Airport;
+import com.driver.model.City;
+import com.driver.model.Flight;
+import com.driver.model.Passenger;
+import com.driver.repositories.AirportRepository;
+//import io.swagger.models.auth.In;
+import java.util.Date;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AirportService {
+    @Autowired
+    private AirportRepository airportRepositoryObj;
+
+    public void addAirport(Airport airport) {
+        airportRepositoryObj.addAirport(airport);
+    }
+
+    public String getLargestAirportName() {
+        List<Airport> list = airportRepositoryObj.getAllAirports();
+        list.sort((a, b) -> {
+            if (a.getNoOfTerminals() == b.getNoOfTerminals())
+                return a.getAirportName().compareTo(b.getAirportName());
+
+            return b.getNoOfTerminals() - a.getNoOfTerminals();
+        });
+
+        if (list.isEmpty())
+            return null;
+
+        return list.get(0).getAirportName();
+    }
+
+    public double getShortestDurationOfPossibleBetweenTwoCities(City fromCity, City toCity){
+        List<Flight> list = airportRepositoryObj.getAllFlights();
+
+        double shortestDuration = Double.MAX_VALUE;
+
+        for (Flight flight : list)
+            if (flight.getFromCity() == fromCity && flight.getToCity() == toCity)
+                if (flight.getDuration() < shortestDuration)
+                    shortestDuration = flight.getDuration();
+
+        if (shortestDuration == Double.MAX_VALUE) return -1;
+
+        return shortestDuration;
+    }
+
+    public int getNumberOfPeopleOn(Date date, String airportName) {
+        int count = 0;
+        Airport airport = airportRepositoryObj.getAirportByName(airportName);
+        List<Flight> list = airportRepositoryObj.getAllFlights();
+
+        for (Flight flight : list)
+            if (flight.getFlightDate() == date && (flight.getFromCity() == airport.getCity() || flight.getToCity() == airport.getCity()))
+                ++count;
+
+        return count;
+    }
+
+    public int calculateFlightFare(Integer flightId) {
+        return 3000 + 50 * airportRepositoryObj.getPassengersByFlight(flightId).size();
+    }
+
+    public String bookATicket(Integer flightId, Integer passengerId) {
+        Flight flight = airportRepositoryObj.getFlightById(flightId);
+        List<Integer> passengerList = airportRepositoryObj.getPassengersByFlight(flightId);
+
+        if (airportRepositoryObj.getPassengerById(passengerId) == null || passengerList.contains(passengerId) || passengerList.size() == flight.getMaxCapacity())
+            return "FAILURE";
+
+        airportRepositoryObj.bookTicket(flightId, passengerId);
+
+        return "SUCCESS";
+    }
+
+    public String cancelATicket(Integer flightId, Integer passengerId) {
+        Flight flight = airportRepositoryObj.getFlightById(flightId);
+        if (flight == null || airportRepositoryObj.getPassengerById(passengerId) == null)
+            return "FAILURE";
+
+        List<Integer> passengerList = airportRepositoryObj.getPassengersByFlight(flightId);
+        if (!passengerList.contains(passengerId))
+            return "FAILURE";
+
+        airportRepositoryObj.cancelTicket(flightId, passengerId);
+
+        return "SUCCESS";
+    }
+
+    public int countOfBookingsDoneByPassengerAllCombined(Integer passengerId) {
+        int count = 0;
+        List<Flight> flights = airportRepositoryObj.getAllFlights();
+
+        for (Flight flight : flights)
+            if (airportRepositoryObj.getPassengersByFlight(flight.getFlightId()).contains(passengerId))
+                ++count;
+
+        return count;
+    }
+
+    public String addFlight(Flight flight) {
+        airportRepositoryObj.addFlight(flight);
+
+        return "SUCCESS";
+    }
+
+    public String getAirportNameFromFlightId(Integer flightId) {
+        Flight flight = airportRepositoryObj.getFlightById(flightId);
+        if (flight == null)
+            return null;
+
+        List<Airport> airportList = airportRepositoryObj.getAllAirports();
+        for (Airport airport : airportList)
+            if (airport.getCity() == flight.getFromCity())
+                return airport.getAirportName();
+
+        return null;
+    }
+
+    public int calculateRevenueOfAFlight(Integer flightId) {
+        int size =  airportRepositoryObj.getPassengersByFlight(flightId).size();
+
+        return 3000 * size + 50 * size * (size - 1) / 2;
+    }
+
+    public String addPassenger(Passenger passenger) {
+        airportRepositoryObj.addPassenger(passenger.getPassengerId(), passenger);
+
+        return "SUCCESS";
+    }
+}
